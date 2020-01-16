bb355d781c4e511a10a793aebd3f1544f261c11b
peterg777 <ptrg777@gmail.com>
2020-01-15 21:11:46 -0500

    more html,css,js

---
+++ app.js
@@ -0,0 +1,25 @@
+//friends array
+let friends = ['Rich', 'Marty', 'Brian', 'Christy', 'Lena'];
+
+//loop
+function sing() {
+
+}
+    for (let i = 0; i < friends.length; i++) {
+    
+        for (let j = 99; j >= 1; j--) {
+            if (j > 2) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file ");
+            } else if (j == 2) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file ");
+            } else if (j = 1) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file ");
+
+            }   
+        }
+   }
+        
+        
+      
+
+        
