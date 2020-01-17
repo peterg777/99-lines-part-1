d1e1214f7b1540a16676152c7f5fc60aa47c9e7f
peterg777 <ptrg777@gmail.com>
2020-01-17 14:06:16 -0500

    html and js changes

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 let friends = ['Rich', 'Marty', 'Brian', 'Christy', 'Lena'];
 
 //loop
-function sing() {
 
-}
     for (let i = 0; i < friends.length; i++) {
+
+        console.log(friends[i].toUpperCase() + ':');
     
-        for (let j = 99; j >= 1; j--) {
+        for (let j = 5; j >= 1; j--) {
             if (j > 2) {
-                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file ");
-            } else if (j == 2) {
-                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file ");
-            } else if (j = 1) {
-                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file ");
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file ");
+            } else if (j === 2) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file ");
+            } else if (j === 1) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file ");
 
             }   
         }
